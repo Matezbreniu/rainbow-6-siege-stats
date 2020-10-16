d7707e67cc19ea160a9ea7e92a2c5709a823acd7
Mateusz Bręk <matezbreniu@gmail.com>
2020-10-16 09:33:38 +0200

    feat: Global styles, main theme

---
+++ src/theme/mainTheme.js
@@ -0,0 +1,25 @@
+export const theme = {
+	fontColor: {
+		active: 'hsl(26, 95%, 56%)',
+		white: 'hsl(0, 0%, 100%)',
+		grey: 'hsl(0, 0%, 60%)',
+		black: 'hsl(0, 0%, 10%)',
+	},
+	backgroundColor: {
+		white: 'hsl(0, 0%, 95%)',
+		grey1: 'hsl(0, 0%, 21%)',
+		grey2: 'hsl(0, 0%, 16%)',
+		grey3: 'hsl(0, 0%, 12%)',
+	},
+	fontWeight: {
+		light: 300,
+		bold: 600,
+	},
+	fontSize: {
+		xs: '1.2rem',
+		s: '1.6rem',
+		m: '2.1rem',
+		l: '2.4rem',
+		xl: '4rem',
+	},
+};
